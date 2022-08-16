09f1971f9fa679987a852b65c364090205d2b493
dhcryan <dhc4003@gmail.com>
2022-08-17 00:35:49 +0900

    array final

--- 10.array/pr1.c
+++ 10.array/pr1.c
@@ -56,3 +56,59 @@
 //         }
 //     return 0;
 // }
+
+// selection sort
+//  #include <stdio.h>
+//  #define SIZE 10
+
+// int main(void)
+// {
+//     int list[SIZE] = {3, 4, 5, 2, 6, 7, 9, 8, 1};
+//     int i, j, temp, least;
+
+//     for (i = 0; i < SIZE - 1; i++)
+//     {
+//         least = i;
+//         for (j = i + 1; j < SIZE; j++)
+//         {
+//             if (list[j] < list[least])
+//                 least = j;
+//         }
+//         temp = list[i];
+//         list[i] = list[least];
+//         list[least] = temp;
+//     }
+
+//     for (i = 0; i < SIZE - 1; i++)
+//     {
+//         printf("%3d", list[i]);
+//     }
+//     return 0;
+// }
+
+#include <stdio.h>
+#define YEARS 3
+#define PRODUCTS 5
+
+int main(void)
+{
+    int sales[YEARS][PRODUCTS] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    int total_sale;
+
+    total_sale = sum(sales);
+    printf("%d\n", total_sale);
+    return 0;
+}
+int sum(int scores[YEARS][PRODUCTS])
+{
+    int y, p;
+    int total = 0;
+    for (y = 0; y < YEARS; y++)
+    {
+        for (p = 0; p < PRODUCTS; p++)
+        {
+            total += scores[y][p];
+        }
+    }
+    return total;
+}
